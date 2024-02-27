56f9bfd77c48d054715667b0fa78bb308a26cfed
BeatrizDias <beatriz.dias76@etec.sp.gov.br>
2024-02-26 22:06:53 -0300

    chore. added .gitignore

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        //Declaração de variável: tipo nometipoDaVariavel = 0;
+        int count = 0;
+        string nome = "";
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void onnamecliked(object sender, EventArgs e)
+      
+        {
+            nome = NameEntry.Text;
+            //Interpolação
+            GreetingLabel.Text = $"Hello, {nome}";
+
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+
+
+    }
+
+
+
+
+}
